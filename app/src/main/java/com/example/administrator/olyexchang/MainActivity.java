6e7927b72618aa5a7b4022e78eac3d2aa55c643a
oly <slipkntoly@gmail.com>
2017-12-03 10:32:59 +0700

    Add Fragement Success

--- app/src/main/java/com/example/administrator/olyexchang/MainActivity.java
+++ app/src/main/java/com/example/administrator/olyexchang/MainActivity.java
@@ -3,11 +3,24 @@ package com.example.administrator.olyexchang;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.administrator.olyexchang.fragement.MainFragement;
+
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragement to Activity
+        if (savedInstanceState == null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentMainFragement,new MainFragement())
+                    .commit();
+        }
+
+
+    }// Main Method
+}//MainActivity Class
